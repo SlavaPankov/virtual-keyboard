a4fcf72c940fcb1efbe9395ff055f39c84bf3bc5
Slava <slavapank94@yandex.ru>
2023-04-30 18:01:05 +0600

    feat: implement KeyboardModel constructor

--- src/js/components/KeyBoard/model/KeyboardModel.js
+++ src/js/components/KeyBoard/model/KeyboardModel.js
@@ -1,5 +1,23 @@
-class KeyboardModel {
+import keys from '../../../config';
 
+class KeyboardModel {
+  constructor() {
+    this.data = keys;
+    this.engKeys = ['key', 'shiftKeyEN'];
+    this.ruKeys = ['keyRu', 'shiftKeyRu'];
+    this.shifted = false;
+    this.capsed = false;
+    this.language = localStorage.getItem('language') || false;
+    this.dataValues = this.language ? this.getValueByKey('keyRu') : this.getValueByKey('key');
+  }
+
+  saveLanguageToLS() {
+    localStorage.setItem('language', this.language);
+  }
+
+  getValueByKey(key) {
+    return this.data.map((item) => (item.mode ? item.mode === 'additional' && item.key : item[key]));
+  }
 }
 
 export default KeyboardModel;
